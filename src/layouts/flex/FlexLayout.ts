71ef6702d555868e10370652353a2af19ce3cc9a
Pavel <>
2023-06-12 16:01:07 +0300

    feat: update logic for flex layout size calculation

--- src/layouts/flex/FlexLayout.ts
+++ src/layouts/flex/FlexLayout.ts
@@ -1,9 +1,11 @@
 import { BaseLayout } from '../BaseLayout';
-
+import { DisplayObject } from '../../display';
 import { TFlexDirection, TFlexJustifyContent, TFlexAlignItems } from './types';
 
 export class FlexLayout extends BaseLayout {
   protected _gap = 0;
+  protected _horizontalSize = 0;
+  protected _verticalSize = 0;
 
   public direction: TFlexDirection;
   public justifyContent: TFlexJustifyContent;
@@ -17,6 +19,26 @@ export class FlexLayout extends BaseLayout {
     this.alignItems = 'flex-start';
   }
 
+  appendChild(child: DisplayObject): void {
+    this.children.push(child);
+
+    // It's looks like bad practice, but link to parent have to be added here
+    child.parent = this;
+    child.updatePosition();
+    this.update();
+  }
+
+  removeChild(child: DisplayObject): void {
+    const index = this.children.indexOf(child);
+    if (index === -1) return;
+
+    this.children.splice(index, 1);
+
+    // It's looks like bad practice, but link to parent have to be removed here
+    child.parent = null;
+    this.update();
+  }
+
   update() {
     const mainAxisSizeAttribute = this.direction === 'row' || this.direction === 'row-reverse' ? 'width' : 'height';
     const elementsTotalSize = this.children.reduce((accumulator, item) => accumulator + item[mainAxisSizeAttribute], 0);
@@ -24,9 +46,9 @@ export class FlexLayout extends BaseLayout {
     const crosAxisSizeAttribute = this.direction === 'row' || this.direction === 'row-reverse' ? 'height' : 'width';
     const crossAxisMaxElementSize = this.children.reduce((accumulator, item) => Math.max(accumulator, item[crosAxisSizeAttribute]), 0);
 
-    const containerMainAxisTotalSize = this.direction === 'row' || this.direction === 'row-reverse' ? this._width : this._height;
+    const containerMainAxisTotalSize = this.direction === 'row' || this.direction === 'row-reverse' ? this._horizontalSize : this._verticalSize;
     const containerCrossAxisTotalSize = Math.max(
-      this.direction === 'row' || this.direction === 'row-reverse' ? this._width : this._height,
+      this.direction === 'row' || this.direction === 'row-reverse' ? this._horizontalSize : this._verticalSize,
       crossAxisMaxElementSize,
     );
 
@@ -78,9 +100,9 @@ export class FlexLayout extends BaseLayout {
       }
     }
 
-    const contentSize = elementsTotalSize + (this.children.length - 1) * this._gap;
-    this._width ||= this.direction === 'row' || this.direction === 'row-reverse' ? contentSize  : crossAxisMaxElementSize;
-    this._height ||= this.direction === 'row' || this.direction === 'row-reverse' ? crossAxisMaxElementSize : contentSize;
+    const contentSize = initialGap + elementsTotalSize + (this.children.length - 1) * this._gap;
+    this._width = this.direction === 'row' || this.direction === 'row-reverse' ? contentSize  : crossAxisMaxElementSize;
+    this._height = this.direction === 'row' || this.direction === 'row-reverse' ? crossAxisMaxElementSize : contentSize;
   }
 
   get gap() {
@@ -91,5 +113,18 @@ export class FlexLayout extends BaseLayout {
       this._gap = value;
       this.update();
     }
+  }
+
+  get width(): number {
+    return this._horizontalSize || this._width;
+  }
+  set width(value: number) {
+    this._horizontalSize = value;
+  }
+  get height(): number {
+    return this._verticalSize || this._height;
+  }
+  set height(value: number) {
+    this._verticalSize = value;
   }
 }
